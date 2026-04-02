64bf810d6b3cf098546136a3b4e8291789cdd934
joePyDev <mailalertforex@gmail.com>
2026-04-02 15:03:04 +0200

    update

---
+++ How Do Functions Work in Python/Function ex1.py
@@ -0,0 +1,41 @@
+
+
+
+name = input("What is your name?")
+print("Hello",name)
+
+
+
+print(int(3.14))
+print(int("42"))
+print(int(True))
+print(int(False))
+
+
+
+def hello():
+    print("Hello Word")
+    
+def calculate_sum(a,b):
+    print(a + b)
+
+calculate_sum(3,1)
+
+my_sum = calculate_sum(3, 1)
+print(my_sum) # none
+
+
+
+
+def calculate_sum2(a,b):
+    return a + b
+
+my_sum = calculate_sum2(3, 1)
+print(my_sum)
+
+
+#----------------
+
+
+# scope LEGB
+
