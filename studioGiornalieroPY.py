da661d9c658394efc460fb92d56840d846751f36
joePyDev <mailalertforex@gmail.com>
2026-04-09 15:18:50 +0200

    update

--- studioGiornalieroPY.py
+++ studioGiornalieroPY.py
@@ -1,4 +1,4 @@
-
+"""
 import sys
 
 # lavorare con i file
@@ -15,6 +15,73 @@ for line in fhandle:
     if line.startswith("From"):
         count = count + 1
 print(f"corispondenza trovata {count} volte nel file {fname} ")        
+
+"""
+
+
+
+fout = open(r"C:\Users\gioel\OneDrive\Desktop\nuovo_file.txt","w")
+print(fout)
+
+line = "contenuto da scrivere\n"
+fout.write(line)
+fout.write("1234.\n")
+fout.write("1234..\n")
+fout.write("1234...\n")
+fout.write("1234....\n")
+
+fout.close()
+
+
+
+#-------------------------------- liste
+
+vuoto = []
+
+pieno = [1,"stringa",False]
+
+
+# le liste sono mutabili
+print(id(pieno))
+
+
+# paretesi a sinistra, modifica del elemento:
+pieno[1] = "formaggio"
+print(id(pieno))
+
+A = pieno
+# ATTENZIONE!! 
+A.append("11111")
+
+print(A)
+print(id(pieno))
+print(pieno)
+
+
+
+
+# scorrere un elenco: 
+
+for contenuto in pieno:
+    print(contenuto)
+
+
+
+
+numeri = [10,15,20,25]
+myList = []
+
+
+for i in numeri:
+    myList.append(i)        
+
+print(myList)
+
+
+
+
+
+
 
 
 
