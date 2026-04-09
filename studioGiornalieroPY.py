719254d9fe012d365989c0d863af2531af73faf4
joePyDev <mailalertforex@gmail.com>
2026-04-09 12:58:43 +0200

    update

--- studioGiornalieroPY.py
+++ studioGiornalieroPY.py
@@ -1,16 +1,20 @@
 
+import sys
 
 # lavorare con i file
 
-
 fname = input("inserisci il nome del file : ")
-fhandle = open(fname)
+try:
+    fhandle = open(fname)
+except:
+    print(" il file non può essere aperto")
+    sys.exit()   
+    
 count = 0
 for line in fhandle:
     if line.startswith("From"):
         count = count + 1
-print(f"corispondenza trovata {count} volte nel file {fhandle} ")        
-
+print(f"corispondenza trovata {count} volte nel file {fname} ")        
 
 
 
