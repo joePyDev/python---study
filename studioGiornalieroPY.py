3dcaf3e35aa93057733cdfa968e2755aa5242bd9
joePyDev <mailalertforex@gmail.com>
2026-04-12 11:49:05 +0200

    update

--- studioGiornalieroPY.py
+++ studioGiornalieroPY.py
@@ -106,17 +106,90 @@ print(newT)
 
 help(str.split)
 
-"""
 
 
 fhand = input("inserisci il percorso file")
 with open(fhand) as myFile:
+    print(myFile,"!!!")
     for line in myFile:
         line = line.rstrip()
         if not line.startswith(("From")): continue
         words = line.split()
         if len(words) > 3:
             print(words[2])
+
+
+
+"""
+
+
+
+
+# oggetti e valori
+
+
+# unico oggetto a cui fanno riferimento 2 etichette
+A = "stringa"
+B = "stringa"
+
+print(A is B) # true
+
+
+
+# creando 2 liste invece si crea 2 oggetti
+
+a = [1,2,3]
+b = [1,2,3]
+
+print(a is b) # false
+
+# un oggetto ha un valore che è una sequenza di eelementi
+
+
+
+C = (1,2,3)
+D = (1,2,3)
+
+print(C is D)
+
+
+
+
+# alias
+# Se a si riferisce ad un oggetto e assegni b = a, allora entrambe le variabili si
+# riferiscono allo stesso oggetto:
+
+A = [1,2,3]
+
+B = A
+
+B.append(5) # modifica in-place tutti gli alias vedono la modifica
+
+print(A)
+
+print(B is A , "flag")
+
+# Se l’oggetto con alias è modificabile, le modifiche apportate su un alias si riflettono
+# sugli altri:
+
+
+# Sebbene questo comportamento possa essere utile, è spesso fonte di errori. In
+#generale, è meglio evitare gli alias quando si lavora con oggetti mutabili.
+prezzi_originali = [100, 200, 300]
+prezzi_scontati = prezzi_originali.copy() # COPIA
+
+# Modifico la copia
+for i in range(len(prezzi_scontati)):
+    prezzi_scontati[i] *= 0.9
+    print("Scontato:",prezzi_scontati[i] , "e prezzo originale" , prezzi_originali[i])
+
+print(prezzi_originali) # [100, 200, 300] -> SALVO!
+
+
+
+
+
+
 
 
 
