54c90eda34a8ab779233a96e55af5767bd239593
joePyDev <mailalertforex@gmail.com>
2026-04-22 16:04:44 +0200

    update

--- studioGiornalieroPY.py
+++ studioGiornalieroPY.py
@@ -3,65 +3,64 @@
 # help()
 
 
-"""
-
-counts = {}
-
-counts['apple'] = counts.get('apple', 0) + 1
-counts['apple'] = counts.get('apple', 0) + 1
-counts['apple'] = counts.get('apple', 0) + 1
-
-print(counts)
 
 
 
-words = ['apple', 'banana', 'apple']
-
-counts = dict()
-
-for w in words:
-    counts[w] = 10
-
-print(counts)
+x = { 'chuck' : 1 , 'fred' : 42, 'jan': 100}
+y = x.items()
 
 
+print(y)
+days = ('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
+print(days[2])
 
-lines = ["red blue", "green yellow"]
-
-for line in lines:
-    words = line.split()
-    for w in words:
-        print(w)
-
-
-line = "clown ran away"
-words = line.split()
-print(words)
+X = ("glen","sally","joseph")
+print(X[1])
+X = (1,2,3,4,5,6)
+print(X)
+print(max(X))
 
 
 
-counts = {'apple': 3, 'banana': 5, 'cherry': 2}
-
-largest = None
-for word, count in counts.items():
-    if largest is None or count > largest:
-        largest = count
-
-print(largest)
-
-"""
 
 
+(x , y) = ( 4 , 5 )
 
-# Sfida di programmazione funzionale: Calcolatrice di prezzi
+print(x)
+print(y)
 
-sconto = 15
-prezzo_articolo = 75 
 
-valore_sconto = sconto / 100 * prezzo_articolo
-prezzo_scontato = prezzo_articolo - valore_sconto
+d = dict()
+d["csev"] = 2
+d["cwen"] = 4
 
-print(f"il prezzo scontato è: {prezzo_scontato}")  
+for (k,v) in d.items():
+    print(k,v)
+    
+tups = d.items()
+print(tups)    
+
+d = {"a":10 , "f":123,"c": 12 }
+
+print(d.items())
+print(sorted(d.items()))
+
+
+d = {"a":10 , "f":123,"c": 12 }
+diz = dict()
+for k,v in sorted(d.items()):
+    print(k,v)
+    diz[k] = v
+    print(diz)
+
+
+c = {"a":12,"B":14,"C":45,"s":36}
+tmp = list()
+for k,v in c.items():
+    tmp.append((v,k))
+print(sorted(tmp , reverse = True))
+
+
 
 
 
