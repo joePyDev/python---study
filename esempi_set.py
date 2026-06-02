a8cc3c81ecfe98e5c8377a723039d4fe07dc7276
joePyDev <mailalertforex@gmail.com>
2026-06-02 19:41:02 +0200

    update

---
+++ esempi_set.py
@@ -0,0 +1,39 @@
+# set
+
+responses = ["yes", "no", "yes", "maybe", "no", "yes"]
+unique_responses = set(responses)
+
+print("yes" in unique_responses, "always" in unique_responses, "maybe" in unique_responses)
+
+
+numbers = [4, 4, 2, 2, 4]
+unique_numbers = set(numbers)
+print(unique_numbers)
+
+
+
+group_a = {"alice", "bob", "carol"}
+group_b = {"bob", "dave"}
+group_c = {"carol", "eve"}
+
+merged = group_a.union(group_b)
+merged = merged.union(group_c)
+
+print(merged)
+
+
+
+
+
+
+survey_a = ["blue", "green", "blue", "red", "green"]
+survey_b = ["yellow", "blue", "yellow", "red"]
+
+clean_a = set(survey_a)
+clean_b = set(survey_b)
+
+all_unique = clean_a.union(clean_b)
+print(all_unique)
+
+
+
---
+++ parser_xml.py
@@ -0,0 +1,36 @@
+# Devi esaminare tutti i tag <comment> e trovare la somma dei valori <count>.
+
+"""
+<commento>
+  <nome>Mattia</nome>
+  <count>97</count>
+</comment>
+"""
+
+
+
+import urllib.request
+import xml.etree.ElementTree as ET
+
+url = input('Enter location: ')
+if len(url) < 1 : 
+    url = 'http://py4e-data.dr-chuck.net/comments_42.xml'
+
+print('Retrieving', url)
+uh = urllib.request.urlopen(url)
+data = uh.read()
+print('Retrieved',len(data),'characters')
+tree = ET.fromstring(data)
+
+counts = tree.findall('.//count')
+nums = list()
+somma = 0
+for result in counts:
+    nums.append(result.text)
+    somma = somma + int(result.text)
+
+print('Count:', len(nums))
+print('Sum:', somma)
+
+
+
