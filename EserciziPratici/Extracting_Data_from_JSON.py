2ab223405dca0ceff83f6a5468f59e128611a0f3
joePyDev <mailalertforex@gmail.com>
2026-06-09 17:39:11 +0200

    update

---
+++ EserciziPratici/Extracting_Data_from_JSON.py
@@ -0,0 +1,35 @@
+ # Extracting Data from JSON
+
+"""
+l programma richiederà un URL, 
+leggerà i dati JSON da quell'URL utilizzando urllib, 
+quindi analizzerà ed estrarrà il numero di commenti dai dati JSON, 
+calcolerà la somma dei numeri nel file e inserirà la somma qui sotto.
+
+"""
+
+url1 = "http://py4e-data.dr-chuck.net/comments_42.json"
+url2 = "http://py4e-data.dr-chuck.net/comments_2383209.json"
+
+
+import urllib.request
+import json
+
+url = input("inserisci url: ")
+if len(url) < 1:
+    url = url2
+
+apertura_url = urllib.request.urlopen(url)
+data = apertura_url.read()
+contenuto_json = json.loads(data)
+    
+lista_valori = []
+for chiave in contenuto_json["comments"]:
+    lista_valori.append(chiave["count"])
+    
+    
+lista_valori = sum(lista_valori)   
+print(lista_valori)  
+    
+    
+    
---
+++ EserciziPratici/inventory.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+# Load inventory data from CSV
+inventory_df = pd.read_csv('inventory.csv')
+
+# Set the 'product_name' column as the index
+inventory_df.set_index('product_name', inplace=True)
+
+# Convert the DataFrame directly to a dictionary, selecting only the 'stock_level' column
+inventory_dict = inventory_df['stock_level'].to_dict()
+
+# Print the inventory
+print("Initial Inventory:")
+print(inventory_dict)
+
+
+# Add "Puppy Snacks" to the inventory
+inventory_dict["Puppy Snacks"] = 40
+
+# Update "Cheesy Chompers" in the inventory and assign it the value of 20
+inventory_dict["Cheesy Chompers"] = 20
+
+print("\nUpdated Inventory:")
+print(inventory_dict)
+
+
+# We've discontinued "Peanut Butter Biscuits". Remove it from the inventory
+
+# Remove item 
+del inventory_dict["Peanut Butter Biscuits"] 
+
+print("\nUpdated Inventory after Discontinuation:")
+print(inventory_dict)
+
+
+print("\nInventory Report:")
+for product, stock in inventory_dict.items(): 
+    print(f"Product: {product}, Stock: {stock}") 
+
+
